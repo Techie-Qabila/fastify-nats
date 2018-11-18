77da48799f7c3bc23bfa44f41da692d7578e0ee4
Sandro Martini <sandro.martini@gmail.com>
2018-11-18 16:17:20 +0100

    small update in tests

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const Fastify = require('fastify')
 const fastifyNats = require('./index')
 
 const natsOpt = {
-  url: 'nats:demo.nats.io:4222'
+  url: 'nats://demo.nats.io:4222'
 }
 
 test('fastify.nats should exist', t => {
@@ -14,11 +14,11 @@ test('fastify.nats should exist', t => {
 
   const fastify = Fastify()
 
-  fastify.register(fastifyNats, natsOpt, err => {
+  fastify.register(fastifyNats, natsOpt, (err) => {
     t.error(err)
   })
 
-  fastify.ready(err => {
+  fastify.ready((err) => {
     t.error(err)
     t.ok(fastify.nats)
 
