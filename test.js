e291e85c1843b0389203d69b4239cd0b313d241a
Sandro Martini <sandro.martini@gmail.com>
2018-12-06 11:38:22 +0100

    Update to latest fastify 1.x and all dependencies (#1)
    * update dependencies to latest Fastify 1.x and all plugins to latest release
    
    * small update in tests
    
    * update plugin implementation using Fastify 1.x features/syntax, and related tests
    
    * small update
    
    * update dependencies

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const Fastify = require('fastify')
 const fastifyNats = require('./index')
 
 const natsOpt = {
-  url: 'nats:demo.nats.io:4222'
+  url: 'nats://demo.nats.io:4222'
 }
 
 test('fastify.nats should exist', t => {
@@ -14,12 +14,11 @@ test('fastify.nats should exist', t => {
 
   const fastify = Fastify()
 
-  fastify.register(fastifyNats, natsOpt, err => {
-    t.error(err)
-  })
+  fastify.register(fastifyNats, natsOpt)
 
-  fastify.ready(err => {
+  fastify.ready((err) => {
     t.error(err)
+    t.ok(fastify.hasDecorator('nats'))
     t.ok(fastify.nats)
 
     fastify.close()
